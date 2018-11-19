bd7798f6de7bf5108bbd5f9fba2ea52a7b319822
WHITEHEAD <twhitehead.1991@gmail.com>
2018-11-19 12:25:16 +0000

    updated requirements.txt to match venv

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ def get_tracklist(url):
     tracklist = False
     website = urlparse(url).hostname.split('.')[1]
     if website == 'bbc':
-        bbc = Bbc()
-        tracklist = bbc.get_data(url = url)
+        instance = Bbc()
     elif website == 'nts':
-        nts = Nts()
-        tracklist = nts.get_data(url = url)
+        instance = Nts()
     
+    tracklist = instance.get_data(url)
     return tracklist
 
     
 if __name__ == "__main__":
     
-    shows = ['https://www.bbc.co.uk/programmes/m0000pc1', 
-             'https://www.nts.live/shows/moxie/episodes/moxie-10th-october-2018',
-             'https://stackoverflow.com/questions/1663807/how-to-iterate-through-two-lists-in-parallel']
+    shows = ['https://www.nts.live/shows/ygg/episodes/ygg-15th-november-2018',
+             'https://www.nts.live/shows/real-bitch-radio/episodes/realbitchradio-w-violet-waters-15th-november-2018',
+             'https://www.nts.live/shows/naomi/episodes/naomi-aaron-j-14th-november-2018',
+             'https://www.bbc.co.uk/programmes/m00015b0']
     
     for idx, url in enumerate(shows):
         tracklist = get_tracklist(url)
